09b7d62a56c145e6ca000b57e8c25d8d4ff58b11
Taz Campbell <shelbyacampbell2724@gmail.com>
2024-02-15 15:33:27 -0500

    added some routes and imported to server

--- controllers/bookmark.js
+++ controllers/bookmark.js
@@ -1,1 +1,36 @@
-//place holder for routes
+const express = require('express')
+const Bookmarks = require('../models/Bookmark')
+
+const router = express.Router()
+
+router.get('/', async (req, res) => {
+    try{
+        res.json(await Bookmarks.find({}))
+    } catch (err) {
+        res.status(400).json(err)
+    }
+})
+
+router.post ('/', async (req, res) => {
+    try{
+        res.json(await Bookmarks.create(req.body))
+    } catch (err) {
+        res.status(400).json(err)
+    }
+})
+
+router.delete('/:id', async (req, res) => {
+    try{
+        res.json(await Bookmarks.findByIdAndDelete(req.params.id))
+    } catch (err) {
+        res.status(400).json(err)
+    }
+})
+
+
+
+
+
+
+
+module.exports = router
