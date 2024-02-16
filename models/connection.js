a4cab395f6b33836a141980e02fc7c9d9b962613
Christian Alexander <christian.alexanderato@gmail.com>
2024-02-15 16:14:40 -0800

    comments added to connection.js

--- models/connection.js
+++ models/connection.js
@@ -1,6 +1,9 @@
+// Dependencies Used
 require("dotenv").config();
 const mongoose = require("mongoose");
 
+// Established Database Connection
 mongoose.connect(process.env.DATABASE_URL);
 
+// Export Connection 
 module.exports = mongoose;
